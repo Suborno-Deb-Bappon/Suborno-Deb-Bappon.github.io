51d21d3e5bab907db4e1dcd697edfea57b67a27b
Suborno Deb Bappon <subornodebbappon20@gmail.com>
2026-09-19 00:36:58 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-889ad1be3e457f293a30.js"
+    "url": "webpack-runtime-08cb5bd9ad06c49bfab6.js"
   },
   {
     "url": "framework-f09f45822e9ba8fc378b.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c9d70c8105a4ca4730f482a712d0224a"
+    "revision": "8aec7229cb6beee7340de9c70b9ad695"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0eefb2092d535e2a0e88.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f9f860c7cfb53dd7b542ce9ed400677c"
+    "revision": "18ed237578c94099362464fe9eae9e72"
   },
   {
     "url": "manifest.webmanifest",
